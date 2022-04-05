30477d496254ac5db6173a2721b09c88dfd6ecab
Pablo Muñoz <26773480+munozp@users.noreply.github.com>
2022-04-05 18:01:43 +0200

    Creado proyecto
    Creado proyecto en NetBeans y prueba de la clase Tool.

--- src/rars/venus/HelpAboutAction.java
+++ src/rars/venus/HelpAboutAction.java
@@ -52,7 +52,8 @@ public class HelpAboutAction extends GuiAction {
                         "  *  Tango Desktop Project (tango.freedesktop.org),\n" +
                         "  *  glyFX (www.glyfx.com) Common Toolbar Set,\n" +
                         "  *  KDE-Look (www.kde-look.org) crystalline-blue-0.1,\n" +
-                        "  *  Icon-King (www.icon-king.com) Nuvola 1.0.",
+                        "  *  Icon-King (www.icon-king.com) Nuvola 1.0.\n" + 
+                        "\nModified version by Pablo Muñoz (UAH)\nfor the Degree on Space Physics and Instrumentation.",
                 "About Rars",
                 JOptionPane.INFORMATION_MESSAGE,
                 new ImageIcon("images/RISC-V.png"));
